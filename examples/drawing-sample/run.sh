3c2414f56f48ca78cf3d8d0423ab08cf7039175b
Michael Kargl <michael.kargl@zuehlke.com>
2023-10-27 12:30:33 +0200

    chore: makes run.sh more resilient to path changes

--- examples/drawing-sample/run.sh
+++ examples/drawing-sample/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-git submodule foreach --recursive 'git submodule init && git submodule update'
+script_path="$0"
+script_dir="$(dirname $script_path)"
 
+
+cd "$script_dir"
+git submodule foreach --recursive 'git submodule init && git submodule update'
 platformio run --target upload --environment native
+cd -
